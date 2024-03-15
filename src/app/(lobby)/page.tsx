63572e10ff97a89cdcbdb58e61d6bb44face744a
saufth <saufth@gmail.com>
2024-03-15 14:35:27 -0600

    Refactor service icons in lobby page

--- src/app/(lobby)/page.tsx
+++ src/app/(lobby)/page.tsx
@@ -5,6 +5,14 @@ import { services } from '@/config/organization'
 import { Card, CardContent, CardHeader } from '@/components/ui/card'
 import { Icons } from '@/components/icons'
 
+const serviceIconStyle = 'w-auto h-48 xl:h-64 mx-auto fill-transparent stroke-accent stroke-[4px]'
+
+const servicesIcons = [
+  () => <Icons.Instalations className={serviceIconStyle} />,
+  () => <Icons.Maintenance className={serviceIconStyle} />,
+  () => <Icons.DesignEngineering className={serviceIconStyle} />
+]
+
 export default function IndexPage () {
   return (
     <>
@@ -38,31 +46,34 @@ export default function IndexPage () {
             </div>
           </div>
           <div className='cols-container mt-spacing-7 gap-y-gutter lg:gap-y-0'>
-            {services.map((item, key) => (
-              <Card
-                key={key}
-                as='article'
-                className='group w-8-cols lg:w-4-cols bg-black rounded-3xl relative p-0.5'
-              >
-                <span className='h-4 absolute inset-x-0 top-0 overflow-hidden rounded-full'>
-                  <span className='absolute inset-0 rounded-full bg-[image:radial-gradient(75%_100%_at_50%_0%,rgba(180,25,25,0.6)_0%,rgba(180,25,25,0)_75%)] opacity-60 transition-opacity duration-500 group-hover:opacity-100' />
-                </span>
-                <div className='w-full h-full relative z-10 bg-black/60 group-hover:bg-black/50 rounded-3xl p-gutter transition-colors duration-300'>
-                  <CardContent className='grid place-content-center py-20'>
-                    <Icons.Logomark className='w-auto h-36' />
-                  </CardContent>
-                  <CardHeader>
-                    <h3 className='f-heading-3'>
-                      {item.title}
-                    </h3>
-                    <p className='f-body-1 text-muted-foreground text-balance'>
-                      {item.description}
-                    </p>
-                  </CardHeader>
-                </div>
-                <span className='absolute -bottom-0 left-[1.125rem] h-0.5 w-[calc(100%-2.25rem)] bg-gradient-to-r from-accent/0 via-accent/90 to-accent/0 transition-opacity duration-500 group-hover:opacity-60' />
-              </Card>
-            ))}
+            {services.map((item, key) => {
+              const Icon = servicesIcons[key] || (() => <Icons.ImageOff className={serviceIconStyle} />)
+              return (
+                <Card
+                  key={key}
+                  as='article'
+                  className='group w-8-cols lg:w-4-cols bg-black rounded-3xl relative p-0.5'
+                >
+                  <span className='h-4 absolute inset-x-0 top-0 overflow-hidden rounded-full'>
+                    <span className='absolute inset-0 rounded-full bg-[image:radial-gradient(75%_100%_at_50%_0%,rgba(180,25,25,0.6)_0%,rgba(180,25,25,0)_75%)] opacity-60 transition-opacity duration-500 group-hover:opacity-100' />
+                  </span>
+                  <div className='w-full h-full relative z-10 bg-black/60 group-hover:bg-black/50 rounded-3xl px-2 py-6 transition-colors duration-300 space-y-spacing-7'>
+                    <CardContent>
+                      <Icon />
+                    </CardContent>
+                    <CardHeader>
+                      <h3 className='f-heading-3'>
+                        {item.title}
+                      </h3>
+                      <p className='f-body-1 text-muted-foreground text-balance'>
+                        {item.description}
+                      </p>
+                    </CardHeader>
+                  </div>
+                  <span className='absolute -bottom-0 left-[1.125rem] h-0.5 w-[calc(100%-2.25rem)] bg-gradient-to-r from-accent/0 via-accent/90 to-accent/0 transition-opacity duration-500 group-hover:opacity-60' />
+                </Card>
+              )
+            })}
           </div>
         </div>
       </section>
