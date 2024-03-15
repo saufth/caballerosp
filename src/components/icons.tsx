a1e2eb3cbbefc62634c78fab5b1128bb4641304c
saufth <saufth@gmail.com>
2024-03-15 13:23:36 -0600

    Fix stroke attributes in Whatsapp icon

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -336,9 +336,9 @@ export const Icons = {
         viewBox='0 0 24 24'
         fill='none'
         stroke='currentColor'
-        stroke-width='2'
-        stroke-linecap='round'
-        stroke-linejoin='round'
+        strokeWidth='2'
+        strokeLinecap='round'
+        strokeLinejoin='round'
         {...props}
       >
         <title>Whatsapp</title>
