4d833b6ac206d3752cf3cc5fc37a88d73c55e1dd
saufth <saufth@gmail.com>
2024-03-22 18:53:14 -0600

    Update maintenance page icons

--- src/config/services.tsx
+++ src/config/services.tsx
@@ -7,24 +7,19 @@ export const services: Category[] = [
     slug: '/instalaciones-electricas',
     items: [
       {
-        title: 'Instalación de acometidas eléctricas',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla nec purus feugiat, molestie ipsum et, consequat nunc. Nulla facilisi. Nulla nec purus feugiat, molestie ipsum et, consequat nunc.'
+        title: 'Instalación de acometidas eléctricas'
       },
       {
-        title: 'Instalación de sistemas de alumbrado',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla nec purus feugiat, molestie ipsum et, consequat nunc. Nulla facilisi. Nulla nec purus feugiat, molestie ipsum et, consequat nunc.'
+        title: 'Instalación de sistemas de alumbrado'
       },
       {
-        title: 'Conexión de máquinas',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla nec purus feugiat, molestie ipsum et, consequat nunc. Nulla facilisi. Nulla nec purus feugiat, molestie ipsum et, consequat nunc.'
+        title: 'Conexión de máquinas'
       },
       {
-        title: 'Instalación de subestaciones',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla nec purus feugiat, molestie ipsum et, consequat nunc. Nulla facilisi. Nulla nec purus feugiat, molestie ipsum et, consequat nunc.'
+        title: 'Instalación de subestaciones'
       },
       {
-        title: 'Instalación de tableros de distribución',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla nec purus feugiat, molestie ipsum et, consequat nunc. Nulla facilisi. Nulla nec purus feugiat, molestie ipsum et, consequat nunc.'
+        title: 'Instalación de tableros de distribución'
       }
     ]
   },
@@ -35,99 +30,79 @@ export const services: Category[] = [
     items: [
       {
         title: 'Mantenimiento eléctrico predictivo',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla nec purus feugiat, molestie ipsum et, consequat nunc. Nulla facilisi. Nulla nec purus feugiat, molestie ipsum et, consequat nunc.',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit..ipsum et, consequat nunc.',
         items: [
           {
-            title: 'Análisis de aceite de transformadores',
-            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla nec purus feugiat, molestie ipsum et, consequat nunc. Nulla facilisi. Nulla nec purus feugiat, molestie ipsum et, consequat nunc.'
+            title: 'Análisis de aceite de transformadores'
           },
           {
-            title: 'Monitoreo de parámetros eléctricos',
-            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla nec purus feugiat, molestie ipsum et, consequat nunc. Nulla facilisi. Nulla nec purus feugiat, molestie ipsum et, consequat nunc.'
+            title: 'Monitoreo de parámetros eléctricos'
           },
           {
-            title: 'Cursos de mantenimiento eléctrico',
-            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla nec purus feugiat, molestie ipsum et, consequat nunc. Nulla facilisi. Nulla nec purus feugiat, molestie ipsum et, consequat nunc.'
+            title: 'Cursos de mantenimiento eléctrico'
           },
           {
-            title: 'Estudio de Arc-Flash',
-            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla nec purus feugiat, molestie ipsum et, consequat nunc. Nulla facilisi. Nulla nec purus feugiat, molestie ipsum et, consequat nunc.'
+            title: 'Estudio de Arc-Flash'
           },
           {
-            title: 'Estudio de corto circuito',
-            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla nec purus feugiat, molestie ipsum et, consequat nunc. Nulla facilisi. Nulla nec purus feugiat, molestie ipsum et, consequat nunc.'
+            title: 'Estudio de corto circuito'
           },
           {
-            title: 'Estudio de calidad de la energía',
-            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla nec purus feugiat, molestie ipsum et, consequat nunc. Nulla facilisi. Nulla nec purus feugiat, molestie ipsum et, consequat nunc.'
+            title: 'Estudio de calidad de la energía'
           },
           {
-            title: 'Termografía',
-            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla nec purus feugiat, molestie ipsum et, consequat nunc. Nulla facilisi. Nulla nec purus feugiat, molestie ipsum et, consequat nunc.'
+            title: 'Termografía'
           }
         ]
       },
       {
         title: 'Mantenimiento eléctrico preventivo',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla nec purus feugiat, molestie ipsum et, consequat nunc. Nulla facilisi. Nulla nec purus feugiat, molestie ipsum et, consequat nunc.',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla nec purus feugiat.',
         items: [
           {
-            title: 'Mantenimiento a plantas de energía',
-            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla nec purus feugiat, molestie ipsum et, consequat nunc. Nulla facilisi. Nulla nec purus feugiat, molestie ipsum et, consequat nunc.'
+            title: 'Mantenimiento a plantas de energía'
           },
           {
-            title: 'Mantenimiento a subestaciones',
-            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla nec purus feugiat, molestie ipsum et, consequat nunc. Nulla facilisi. Nulla nec purus feugiat, molestie ipsum et, consequat nunc.'
+            title: 'Mantenimiento a subestaciones'
           },
           {
-            title: 'Prueba a transformadores de potencia',
-            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla nec purus feugiat, molestie ipsum et, consequat nunc. Nulla facilisi. Nulla nec purus feugiat, molestie ipsum et, consequat nunc.'
+            title: 'Prueba a transformadores de potencia'
           },
           {
-            title: 'Prueba a transformadores',
-            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla nec purus feugiat, molestie ipsum et, consequat nunc. Nulla facilisi. Nulla nec purus feugiat, molestie ipsum et, consequat nunc.'
+            title: 'Prueba a transformadores'
           },
           {
-            title: 'Prueba a relevadores',
-            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla nec purus feugiat, molestie ipsum et, consequat nunc. Nulla facilisi. Nulla nec purus feugiat, molestie ipsum et, consequat nunc.'
+            title: 'Prueba a relevadores'
           },
           {
-            title: 'Medición de tierras',
-            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla nec purus feugiat, molestie ipsum et, consequat nunc. Nulla facilisi. Nulla nec purus feugiat, molestie ipsum et, consequat nunc.'
+            title: 'Medición de tierras'
           },
           {
-            title: 'Poliza de servicios',
-            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla nec purus feugiat, molestie ipsum et, consequat nunc. Nulla facilisi. Nulla nec purus feugiat, molestie ipsum et, consequat nunc.'
+            title: 'Poliza de servicios'
           }
         ]
       },
       {
         title: 'Mantenimiento eléctrico correctivo',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla nec purus feugiat, molestie ipsum et, consequat nunc. Nulla facilisi. Nulla nec purus feugiat, molestie ipsum et, consequat nunc.',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla nec purus feugiat.',
         items: [
           {
-            title: 'Cambio de equipo dañado',
-            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla nec purus feugiat, molestie ipsum et, consequat nunc. Nulla facilisi. Nulla nec purus feugiat, molestie ipsum et, consequat nunc.'
+            title: 'Cambio de equipo dañado'
           },
           {
-            title: 'Cambio de aceite a transformador',
-            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla nec purus feugiat, molestie ipsum et, consequat nunc. Nulla facilisi. Nulla nec purus feugiat, molestie ipsum et, consequat nunc.'
+            title: 'Cambio de aceite a transformador'
           },
           {
-            title: 'Reparación de puntos calientes',
-            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla nec purus feugiat, molestie ipsum et, consequat nunc. Nulla facilisi. Nulla nec purus feugiat, molestie ipsum et, consequat nunc.'
+            title: 'Reparación de puntos calientes'
           },
           {
-            title: 'Renta de plantas de emergencia',
-            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla nec purus feugiat, molestie ipsum et, consequat nunc. Nulla facilisi. Nulla nec purus feugiat, molestie ipsum et, consequat nunc.'
+            title: 'Renta de plantas de emergencia'
           },
           {
-            title: 'Inspección interna',
-            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla nec purus feugiat, molestie ipsum et, consequat nunc. Nulla facilisi. Nulla nec purus feugiat, molestie ipsum et, consequat nunc.'
+            title: 'Inspección interna'
           },
           {
-            title: 'Emergencias',
-            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla nec purus feugiat, molestie ipsum et, consequat nunc. Nulla facilisi. Nulla nec purus feugiat, molestie ipsum et, consequat nunc.'
+            title: 'Emergencias'
           }
         ]
       }
@@ -139,28 +114,22 @@ export const services: Category[] = [
     slug: '/diseno-ingenieria-electrica',
     items: [
       {
-        title: 'Cuadro de carga',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla nec purus feugiat, molestie ipsum et, consequat nunc. Nulla facilisi. Nulla nec purus feugiat, molestie ipsum et, consequat nunc.'
+        title: 'Cuadro de carga'
       },
       {
-        title: 'Diagrama unifilar',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla nec purus feugiat, molestie ipsum et, consequat nunc. Nulla facilisi. Nulla nec purus feugiat, molestie ipsum et, consequat nunc.'
+        title: 'Diagrama unifilar'
       },
       {
-        title: 'Diseño de instalaciones',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla nec purus feugiat, molestie ipsum et, consequat nunc. Nulla facilisi. Nulla nec purus feugiat, molestie ipsum et, consequat nunc.'
+        title: 'Diseño de instalaciones'
       },
       {
-        title: 'Gestoría y tramitología',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla nec purus feugiat, molestie ipsum et, consequat nunc. Nulla facilisi. Nulla nec purus feugiat, molestie ipsum et, consequat nunc.'
+        title: 'Gestoría y tramitología'
       },
       {
-        title: 'Lay-Out de instalación',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla nec purus feugiat, molestie ipsum et, consequat nunc. Nulla facilisi. Nulla nec purus feugiat, molestie ipsum et, consequat nunc.'
+        title: 'Lay-Out de instalación'
       },
       {
-        title: 'Memoria de cálculo',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla nec purus feugiat, molestie ipsum et, consequat nunc. Nulla facilisi. Nulla nec purus feugiat, molestie ipsum et, consequat nunc.'
+        title: 'Memoria de cálculo'
       }
     ]
   }
@@ -171,7 +140,7 @@ export const services: Category[] = [
   //   items: [
   //     {
   //       title: 'Actualización de equipos eléctricos',
-  //       description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla nec purus feugiat, molestie ipsum et, consequat nunc. Nulla facilisi. Nulla nec purus feugiat, molestie ipsum et, consequat nunc.'
+  //       description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla nec purus feugiat.'
   //     }
   //   ]
   // }
