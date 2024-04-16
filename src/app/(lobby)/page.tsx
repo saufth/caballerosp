22dad69a57da3fb5109a9250ed5de7873ccfe823
saufth <saufth@gmail.com>
2024-04-16 14:02:01 -0600

    update home hero call to action

--- src/app/(lobby)/page.tsx
+++ src/app/(lobby)/page.tsx
@@ -28,8 +28,8 @@ export default function IndexPage () {
         title={siteConfig.description}
         description='Desde instalaciones en baja tensión hasta proyectos en alta tensión, nuestro equipo altamente capacitado y comprometido se encarga de cada detalle.'
         link={{
-          title: 'Soluciones',
-          href: '/#soluciones'
+          title: 'Ponte en contacto',
+          href: '/contacto'
         }}
         videoSrc='/video/home-hero.mp4'
       />
